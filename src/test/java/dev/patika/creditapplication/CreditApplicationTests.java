a1793b1dbeed8f3a871087f3b38e8128a465538b
isakilikya <isakilikya@gmail.com>
2022-02-04 07:21:02 +0300

    Spring Initializr

---
+++ src/test/java/dev/patika/creditapplication/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.creditapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
